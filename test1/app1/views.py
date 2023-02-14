404611ab6a41a296834ee6e2782110f5bd0d9ac7
dong <dong@dong.com>
2023-02-14 13:58:01 +0800

    test

--- test1/app1/views.py
+++ test1/app1/views.py
@@ -7,4 +7,5 @@ from django.http import HttpRequest, JsonResponse
 def test_git(request: HttpRequest):
     print('git test')
     print('世界那么大，我想去看看')
+    print('test')
     return JsonResponse({"code": 200})
